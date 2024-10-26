8eda13bf58a87743fbb563a049de2c3628f3601e
lruiz49 <lruiz1610@gmail.com>
2024-10-26 16:49:35 -0300

    added isAdmin column to user CreateUserTable.sql

--- CreateUserTable.sql
+++ CreateUserTable.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS user (
     username TEXT UNIQUE NOT NULL,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    isAdmin BIT NOT NULL
 );
 
 INSERT INTO user (username, name, email, password) VALUES ('admin', 'admin', 'admin@admin.com', 'admin');
